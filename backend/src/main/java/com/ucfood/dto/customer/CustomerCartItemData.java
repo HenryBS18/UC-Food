c795897176ec63e5d48556c631b676de369306ae
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-02-07 21:25:19 +0800

    update dto

--- backend/src/main/java/com/ucfood/dto/customer/CustomerCartItemData.java
+++ backend/src/main/java/com/ucfood/dto/customer/CustomerCartItemData.java
@@ -9,12 +9,21 @@ import lombok.Setter;
 @NoArgsConstructor
 public class CustomerCartItemData {
 
+    private int customerCartItemID;
+
+    private int menuID;
+
     private String menuName;
+
+    private int price;
 
     private int itemQuantity;
 
-    public CustomerCartItemData(String menuName, int itemQuantity) {
+    public CustomerCartItemData(int customerCartItemID, int menuID, String menuName, int price, int itemQuantity) {
+        this.customerCartItemID = customerCartItemID;
+        this.menuID = menuID;
         this.menuName = menuName;
+        this.price = price;
         this.itemQuantity = itemQuantity;
     }
 
